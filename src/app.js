6cf48ccb4454c0f03181efcadaed072042fa4fb4
Caitlin <ckeefe90@yahoo.com>
2020-10-19 12:44:56 -0400

    updates to brewery/user names

--- src/app.js
+++ src/app.js
@@ -4,6 +4,9 @@ const morgan = require('morgan')
 const cors = require('cors')
 const helmet = require('helmet')
 const { NODE_ENV, CLIENT_ORIGIN } = require('./config')
+const userRouter = require('./user/user-router')
+const breweryRouter = require('./brewery/brewery-router')
+const { Logger } = require('logger')
 
 const app = express()
 
@@ -23,7 +26,23 @@ app.get('/', (req, res) => {
     res.send('Hello, world!')
 })
 
-app.use(function errorHandler(error, req, res, next) {
+app.use(function validateBearerToken(req, res, next) {
+    const apiToken = process.env.API_TOKEN
+    console.log(apiToken)
+    const authToken = req.get('Authorization')
+
+    if (!authToken || authToken.split(' ')[1] !== apiToken) {
+        Logger.error(`Unauthorized request to path: ${req.path}`);
+        return res.status(401).json({ error: 'Unauthorized request' })
+    }
+    // move to the next middleware
+    next()
+})
+
+app.use('/api/user', userRouter)
+app.use('/api/brewery', breweryRouter)
+
+app.use(function errorHandler(error, req, res) {
     let response
     if (NODE_ENV === 'production') {
         response = { error: { message: 'server error' } }
