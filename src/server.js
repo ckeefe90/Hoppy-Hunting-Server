a1000c576150776e27ff1f18c42ec590b38d4de3
ckeefe90 <ckeefe90@yahoo.com>
2020-10-25 11:41:42 -0400

    Fixed pg dependency

--- src/server.js
+++ src/server.js
@@ -1,17 +1,14 @@
 const knex = require('knex')
 const app = require('./app')
-const { PORT, DATABASE_URL } = require('./config')
-const knexLogger = require('knex-logger')
+const config = require('./config')
 
 const db = knex({
     client: 'pg',
-    connection: DATABASE_URL,
+    connection: config.DATABASE_URL,
 })
 
 app.set('db', db)
 
-app.use(knexLogger(db));
-
-app.listen(PORT, () => {
-    console.log(`Server listening at http://localhost:${PORT}`)
+app.listen(config.PORT, () => {
+    console.log(`Server listening at http://localhost:${config.PORT}`);
 })
